9ca27677b676ec385188b0dff791fb1ea32be058
Wim Leflere <wim.leflere@gmail.com>
2021-04-14 16:23:55 +0200

    Cleanup: fix formatting

--- src/test/test_math_func.cpp
+++ src/test/test_math_func.cpp
@@ -17,48 +17,56 @@ TEST_CASE("LeastCommonMultipleTest - Zero")
 	CHECK(0 == LeastCommonMultiple(600, 0));
 }
 
-TEST_CASE("LeastCommonMultipleTest - FindLCM") {
+TEST_CASE("LeastCommonMultipleTest - FindLCM")
+{
 	CHECK(25 == LeastCommonMultiple(5, 25));
 	CHECK(25 == LeastCommonMultiple(25, 5));
 	CHECK(130 == LeastCommonMultiple(5, 26));
 	CHECK(130 == LeastCommonMultiple(26, 5));
 }
 
-TEST_CASE("GreatestCommonDivisorTest - Negative") {
+TEST_CASE("GreatestCommonDivisorTest - Negative")
+{
 	CHECK(4 == GreatestCommonDivisor(4, -52));
 	// CHECK(3 == GreatestCommonDivisor(-27, 6)); // error - returns -3
 }
 
-TEST_CASE("GreatestCommonDivisorTest - Zero") {
+TEST_CASE("GreatestCommonDivisorTest - Zero")
+{
 	CHECK(27 == GreatestCommonDivisor(0, 27));
 	CHECK(27 == GreatestCommonDivisor(27, 0));
 }
 
-TEST_CASE("GreatestCommonDivisorTest - FindGCD") {
+TEST_CASE("GreatestCommonDivisorTest - FindGCD")
+{
 	CHECK(5 == GreatestCommonDivisor(5, 25));
 	CHECK(5 == GreatestCommonDivisor(25, 5));
 	CHECK(1 == GreatestCommonDivisor(7, 27));
 	CHECK(1 == GreatestCommonDivisor(27, 7));
 }
 
-TEST_CASE("DivideApproxTest - Negative") {
+TEST_CASE("DivideApproxTest - Negative")
+{
 	CHECK(-2 == DivideApprox(-5, 2));
 	CHECK(2 == DivideApprox(-5, -2));
 	CHECK(-1 == DivideApprox(-66, 80));
 }
 
-TEST_CASE("DivideApproxTest, Divide") {
+TEST_CASE("DivideApproxTest, Divide")
+{
 	CHECK(2 == DivideApprox(5, 2));
 	CHECK(3 == DivideApprox(80, 30));
 	CHECK(3 == DivideApprox(8, 3));
 	CHECK(0 == DivideApprox(3, 8));
 }
 
-TEST_CASE("IntSqrtTest - Zero") {
+TEST_CASE("IntSqrtTest - Zero")
+{
 	CHECK(0 == IntSqrt(0));
 }
 
-TEST_CASE("IntSqrtTest - FindSqRt") {
+TEST_CASE("IntSqrtTest - FindSqRt")
+{
 	CHECK(5 == IntSqrt(25));
 	CHECK(10 == IntSqrt(100));
 	CHECK(9 == IntSqrt(88));
